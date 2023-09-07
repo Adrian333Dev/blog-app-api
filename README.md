ee85c0d7651894b98e92bca9a2700e543636cd88
Adrian333Dev <adrian333dev@gmail.com>
2023-09-07 18:29:39 +0400

    app reset

--- README.md
+++
@@ -1,9 +0,0 @@
-# Blog App API
-
-## Description
-
-This is a blog app API inspired by [Dev.to](https://dev.to/). It is built with Django and Django Rest Framework.
-
-## Project setup instructions
-
-1. Create your own `settings.dev.py` in `local` directory.
--- core/project/settings/envvars.py
+++
@@ -1,6 +0,0 @@
-from core.general.utils.collections import deep_update
-from core.general.utils.settings import get_settings_from_env
-
-# globals() is a dictionary of all global variables
-deep_update(globals(),
-            get_settings_from_env(ENV_PREFIX))  # type: ignore # noqa: F821
